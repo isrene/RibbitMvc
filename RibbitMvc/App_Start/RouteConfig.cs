e17bae084b6d46e5426bdc7691fce40646cce44a
ReneDev <renedevtj@gmail.com>
2020-05-08 09:54:28 -0700

    Implemented Route Config.

--- RibbitMvc/App_Start/RouteConfig.cs
+++ RibbitMvc/App_Start/RouteConfig.cs
@@ -13,11 +13,64 @@ namespace RibbitMvc
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            // profile/{action}
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                name: "ProfileDefault",
+                url: "profile/{action}",
+                defaults: new { controller = "profile", action = "index" }
+                );
+
+
+            // account/{action}
+            routes.MapRoute(
+               name: "AccountDefault",
+               url: "account/{action}",
+               defaults: new { controller = "account" }
+               );
+
+            // followers
+            routes.MapRoute(
+               name: "Followers",
+               url: "followers",
+               defaults: new { controller = "home", action = "followers" }
+               );
+
+            // following
+            routes.MapRoute(
+              name: "Following",
+              url: "following",
+              defaults: new { controller = "home", action = "following" }
+              );
+
+            // create
+            routes.MapRoute(
+            name: "Create",
+            url: "create",
+            defaults: new { controller = "home", action = "create" }
             );
+
+            //{username}/{action}
+            routes.MapRoute(
+            name: "UserDefault",
+            url: "{username/action}",
+            defaults: new { controller = "user", action = "index" }
+            );
+
+           
+
+            // root
+            routes.MapRoute(
+            name: "Default",
+            url: "",
+            defaults: new { controller = "home", action = "index" }
+            );
+
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
